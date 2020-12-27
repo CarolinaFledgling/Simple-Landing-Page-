f49aef15f1bafdb6203b51477f97c8ddf56dae75
Karolina Kulinska <karolina.kulinska89@gmail.com>
2020-12-27 18:04:28 +0100

    chore: created dynamic navBar using DOM in Js , created Li elements added classes, created a elements , added a elements to li, li elements added to Document Fragment which doesnt affect the Document, doesnt change performance

--- JS/main.js
+++ JS/main.js
@@ -10,16 +10,20 @@ function createLiElement() {
     const fragment = document.createDocumentFragment();
     sections.forEach((section) => {
         const navListElement = document.createElement('li');
+        navListElement.classList.add('nav__item');
         const idSection = section.id;
         // console.log(idSection);
         const linkElement = document.createElement('a');
         linkElement.href = `#${idSection}`;
         linkElement.textContent = idSection;
         linkElement.classList.add('nav-list__link');
-        console.log(linkElement);
+        // console.log(linkElement);
+        navListElement.appendChild(linkElement)
+        fragment.appendChild(navListElement)
 
     })
-
+    ulList.appendChild(fragment)
+    console.log(ulList)
 }
 
 
