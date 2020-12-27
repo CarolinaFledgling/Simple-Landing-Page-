5a3e120932ed466e352c30cf7f2f83c424326cba
Karolina Kulinska <karolina.kulinska89@gmail.com>
2020-12-27 17:30:19 +0100

    chore: Dynamic Navbar with Js, starting build ul list with DOM-JS, create function createLi Element

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+// *** A dynamic Navbar with Js 
+
+//  Useing DocumentFragment interface because is a lightweight version of the Document, if we make changes to the document fragment it doesnt affect the Document doesnt change performance.
+
+const ulList = document.querySelector('.nav-list');
+const sections = document.querySelectorAll('section')
+
+
+function createLiElement() {
+    const fragment = document.createDocumentFragment();
+    sections.forEach((section) => {
+        const navListElement = document.createElement('li');
+        const idSection = section.id;
+        // console.log(idSection);
+        const linkElement = document.createElement('a');
+        linkElement.href = `#${idSection}`;
+        console.log(linkElement);
+
+    })
+
+}
+
+
+createLiElement()
